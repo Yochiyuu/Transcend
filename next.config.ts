34f0ddc1f353705367dc909f61026ee1d10eabcb
Andrew Reinhart <ndrurei@gmail.com>
2025-11-29 20:03:55 +0700

    fix vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    // Mengabaikan modul-modul yang sering bikin error di library walletconnect/wagmi
+    config.externals.push("pino-pretty", "lokijs", "encoding", "tap");
+    return config;
+  },
 };
 
 export default nextConfig;
