13b222e790addd595e91651cb5f810b21026e89e
Andrew Reinhart <ndrurei@gmail.com>
2025-11-29 20:09:53 +0700

    fix vercel

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    // Mengabaikan modul-modul yang sering bikin error di library walletconnect/wagmi
+    // 1. Abaikan library 'tap' dkk yang bikin error di Turbopack sebelumnya
     config.externals.push("pino-pretty", "lokijs", "encoding", "tap");
+
+    // 2. Abaikan konektor Wagmi yang TIDAK kita pakai (biar gak error Module not found)
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "@base-org/account": false,
+      "@coinbase/wallet-sdk": false,
+      "@gemini-wallet/core": false,
+      "@metamask/sdk": false,
+      "@safe-global/safe-apps-provider": false,
+      "@safe-global/safe-apps-sdk": false,
+      porto: false,
+    };
+
     return config;
   },
 };
